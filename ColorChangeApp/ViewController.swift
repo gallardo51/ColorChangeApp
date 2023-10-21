6de6993cdbb9445cd560e77f4f805a9721b5be69
Alex Sobolev <31108@mail.ru>
2023-10-21 16:21:03 +0300

    Final setup

--- ColorChangeApp/ViewController.swift
+++ ColorChangeApp/ViewController.swift
@@ -23,21 +23,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         colorChangeView.backgroundColor = viewColor
         
-        setSliders()
+        setColor()
         setValue(for: redCountLabel, greenCountLabel, blueCountLabel)
     }
-
+    
     @IBAction func changeColorSliderAction(_ sender: UISlider) {
         
         switch sender {
-        case redSlider:
-            setValue(for: redCountLabel)
+        case redSlider: setValue(for: redCountLabel)
         case greenSlider: setValue(for: greenCountLabel)
         default: setValue(for: blueCountLabel)
         }
+        
         setColor()
     }
     
@@ -50,27 +50,29 @@ extension ViewController {
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1
-            )
+        )
     }
-            
-            private func setValue (for labels: UILabel...) {
-                labels.forEach { label in
-                    switch label {
-                case redCountLabel: label.text = string (from: redSlider)
-                case greenCountLabel: label.text = string(from: greenSlider)
-                default: label.text = string(from: blueSlider)
-                }
+    
+    private func setValue (for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redCountLabel: label.text = string (from: redSlider)
+            case greenCountLabel: label.text = string(from: greenSlider)
+            default: label.text = string(from: blueSlider)
             }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
-            private func setSliders () {
-                let ciColor = CIColor(color: viewColor)
-                
-                redSlider.value = Float(ciColor.red)
-                greenSlider.value = Float(ciColor.green)
-                blueSlider.value = Float(ciColor.blue)
-            }
-            
-            private func string(from slider: UISlider) -> String {
-                String(format: "%.2f", slider.value)
-                }
-            }
+
+// setSliders()
+// private func setSliders () {
+// let ciColor = CIColor(color: viewColor)
+//
+// redSlider.value = Float(ciColor.red)
+// greenSlider.value = Float(ciColor.green)
+// blueSlider.value = Float(ciColor.blue)
+// }
